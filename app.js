b9de997b503ec335591478a754c8485508d22a37
herveleandre <47299858+herveleandre@users.noreply.github.com>
2020-02-03 17:37:03 -0500

    Statements and loops

---
+++ app.js
@@ -0,0 +1,83 @@
+// singleline comments
+
+/* multi
+line
+comments
+*/
+
+// console.log("Hello WOrld");
+// var greeting = 'Hello';
+// console.log (greeting);
+// console.error('this is an error');
+// alert(123);
+
+// var name = 'John Doe'; 
+// console.log(name);
+
+// Primitive Type
+// const name ='John Doe';
+
+// console.log(typeof name);
+
+
+  // const val1 = String(5);
+  // const val2 = 6;
+  // const sum = val1 + val2;
+
+  // console.log (sum);
+  // console.log (typeof sum);
+
+
+
+// Simple Math
+//   const num1 = 100;
+//   const num2 = 50;
+//   let val;
+
+  
+// val = num1 + num2;
+// val = num1 - num2;
+// val = num1 / num2;
+// val = num1 % num2;
+
+// val = Math.floor(Math.random() * 20 + 1)
+//   console.log (val);
+  
+// STRING METHODS AND CONCATENATION
+
+// const firstName = 'Herve';
+// const lastName = 'Leandre';
+// const age = 34;
+
+// val = firstName + lastName;
+
+// // concatenation 'space between open quotations'
+// val = firstName + ' ' + lastName;
+// // apend
+// // val = 'Herve';
+// // val += 'Leandre';
+// // val = 'My name is ' + firstName +' and I\' am  ' + age +' years old';
+
+// // console.log (val);
+
+// ###### Conditional Statements and loops
+
+// for ( var i = 1; i <= 100; i++ )
+// {
+//   if ( i%3 === 0 && i%5 === 0 )
+//   {
+//     console.log(   " FizzBuzz" );
+//   }
+//   else if ( i%3 === 0 ) 
+//   {
+//     console.log( " Fizz" );
+//   }
+//   else if ( i%5 === 0 ) 
+//   {
+//     console.log( " Buzz" );
+//   }
+//   else
+//   {
+//     console.log(i);
+//   }
+// }
